8b2f81262808a6113a05d14fac262ce645c6ffcf
Gabe Hege <pghege@gmail.com>
2021-12-15 14:05:00 -0800

    updated config + setup to support creating init usuer

--- gen-config/setup.sh
+++ gen-config/setup.sh
@@ -3,8 +3,11 @@
 apt-get update && apt-get install -y pwgen
 rm app/env
 cd app
+
+
 COOKIE_SECRET=$(pwgen -1s 32)
 SECRET_KEY=$(pwgen -1s 32)
+REDASH_ADMIN_PASSWORD=$(pwgen -1s 32)
 POSTGRES_PASSWORD=$(pwgen -1s 32)
 REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
 DATABASE_CONNECTION="postgresql://postgres:${POSTGRES_PASSWORD}@ds-postgres:5432/postgres"
@@ -17,6 +20,8 @@ echo "REDASH_REDIS_URL=redis://redis:6379/0" >> env
 echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> env
 echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> env
 echo "REDASH_SECRET_KEY=$SECRET_KEY" >> env
+echo "REDASH_ADMIN_EMAIL=$DEFAULT_EMAIL" >> env
+echo "REDASH_ADMIN_PASSWORD=$REDASH_ADMIN_PASSWORD" >> env
 echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> env
 echo "DATABASE_CONNECTION=$DATABASE_CONNECTION" >> env
 echo "PGADMIN_DEFAULT_EMAIL=user@pgadmin4.web" >> env
