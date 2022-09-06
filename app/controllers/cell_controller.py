20c8136611ff09cb779500c8b55ed22042d87af1
Irving <Scarzer@users.noreply.github.com>
2022-09-06 17:14:50 -0400

    Removing GA from API

--- app/controllers/cell_controller.py
+++ app/controllers/cell_controller.py
@@ -1,5 +1,5 @@
 from app.model import ArchiveOperator
-from app.aio import ArchiveExporter, GAReader
+from app.aio import ArchiveExporter
 from app.archive_cell import ArchiveCell
 from flask import request, jsonify
 import pandas as pd
@@ -36,73 +36,6 @@ def finish(tracker):
         status[tracker] = "FINISHED"
         return {"tracker": tracker, "dataset_id": source[tracker]}, 200
     return {"tracker": "not found", "dataset_id": source[tracker]}, 200
-
-
-def ga_finish(tracker, cell, status):
-    print("Working on", tracker)
-    print("Cell", cell)
-    print("STATUS", status)
-    print("STATUS OF TRACKER", status[tracker])
-    ArchiveOperator().add_cell_to_db(cell)
-    status[tracker] = "FINISHED"
-
-
-def ga_meta_finish(tracker, cell, status):
-    print("Working on", tracker)
-    print("Cell", cell)
-    print("STATUS", status)
-    print("STATUS OF TRACKER", status[tracker])
-    ArchiveOperator().add_meta_to_db(cell)
-
-
-def ga_data_finish(tracker, cell, status):
-    print("Working on", tracker)
-    print("Cell", cell)
-    print("STATUS", status)
-    print("STATUS OF TRACKER", status[tracker])
-    ArchiveOperator().add_ts_to_db(cell)
-
-
-def ga_publish(dataset_id):
-    body = request.json
-    token = body.get('token')
-    tracker = str(uuid.uuid4())
-    status[tracker] = "STARTED"
-    source[tracker] = dataset_id
-    gareader = GAReader(token)
-    metadata, columns = gareader.read_metadata(int(dataset_id))
-    if not metadata:
-        return jsonify(
-            {"message": "object is unreadable, missing a field or incorrect token", "dataset_id": dataset_id})
-    cell_id = metadata[LABEL.CELL_ID.value]
-
-    # Launch task into new thread
-    status[tracker] = "IN_PROGRESS"
-    data = gareader.read_data(int(dataset_id), columns)
-    if not data:
-        return jsonify(
-            {"message": "object is unreadable, missing a field or incorrect token", "dataset_id": dataset_id})
-    data[LABEL.CELL_ID.value] = cell_id
-    data = pd.DataFrame(data=data, columns=data.keys())
-    cell = ArchiveCell(cell_id,
-                       test_type=TEST_TYPE.CYCLE.value,
-                       metadata=metadata,
-                       data=data)
-
-    threading.Thread(target=ga_finish, name="data_thread",
-                     args=(tracker, cell, status)).start()
-    # # Add something from metadata into response
-    return jsonify(
-        {"tracker": tracker, "dataset_id": dataset_id, "token": token})
-
-
-def ga_publish_status(tracker):
-    if tracker in status and tracker in source:
-        return jsonify(
-            {"status": status[tracker], "dataset_id": source[tracker], "tracker": tracker})
-    return jsonify({"status": "Unknown Tracker ID",
-                    "dataset_id": "Unknown", "tracker": "Unknown"})
-
 
 def get_cells():
     """get_cell
